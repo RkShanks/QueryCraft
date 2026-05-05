54ae9660a4e708ec8ef0f4260720525803802c12
RkShanks <zid.omar128885@gmail.com>
2026-05-05 20:51:10 +0300

    fix(infra): correct DO 70749 syntax in postgres role alias script

--- dbTest/init/00-create-postgres-role.sql
+++ dbTest/init/00-create-postgres-role.sql
@@ -3,10 +3,10 @@
 -- Our container uses "source_readonly" as the superuser, so we create "postgres"
 -- as an alias role so schema init succeeds.
 
-DO $
+DO $$
 BEGIN
    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'postgres') THEN
       CREATE ROLE postgres WITH LOGIN SUPERUSER PASSWORD 'source_dev';
    END IF;
 END
-$;
+$$;
