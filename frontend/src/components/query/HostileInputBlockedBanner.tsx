925d19c3d49e0d63a92b352a9b73f004fefcc329
RkShanks <zid.omar128885@gmail.com>
2026-06-23 11:45:14 +0300

    test(T-851): write RED tests for HostileInputBlockedBanner
    FR-158

---
+++ frontend/src/components/query/HostileInputBlockedBanner.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const HostileInputBlockedBanner: React.FC = () => {
+  return <div data-testid="hostile-input-blocked-banner">Skeleton Hostile Input Blocked Banner</div>;
+};
